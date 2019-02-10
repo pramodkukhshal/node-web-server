196b4735f6693ec2fd745ee65d0093dc7f4621b7
Pramod Kukhshal <Pramod@Pramods-MacBook-Pro.local>
2019-02-10 14:13:08 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+} )
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        console.log(err);
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+})
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'About',
+        welcomeMessage: 'Welcome to my website'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'This is bad request'
+    });
+});
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`)
+});
